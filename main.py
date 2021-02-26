f6409ff0880cb21818fd722c042331bdda21cbf8
12skoko <1458987208@qq.com>
2021-02-27 00:34:12 +0800

    update

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ import re
 
 class WeipaiingState():
     def __init__(self):
-        self.works = [[1, '智取威虎山', 0, ''], [2, '芬兰近代银币', 0, ''], [3, '王俭作品', 0, '']]
-        self.worksstate = self.works
+        self.worksstate = []
+        self.worksstate2 = self.worksstate
 
     def ProcessingBiddingMsg(self, input):
-        msg_re = re.compile('(私信?)?(\d+)[号\.+、，,:：加 ](\d{2})')
+        msg_re = re.compile('(私信?)?(\d+)[号\.+、，,:：。加 ]+(\d{2,})')
         try:
             msg_search = re.findall(msg_re, input[0])
         except:
             return -1
         f = len(msg_search)
+        worksstatetemp = self.worksstate2
+        self.worksstate2 = self.worksstate
         for i in msg_search:
-            if int(i[1]) > len(self.works) or int(i[1]) < 1 or int(i[2]) <= 0:
+            if int(i[1]) > len(self.worksstate) or int(i[1]) < 1 or int(i[2]) <= 0:
                 f -= 1
                 continue
             self.worksstate[int(i[1]) - 1][2] = int(i[2]) + self.worksstate[int(i[1]) - 1][2]
@@ -30,33 +32,35 @@ class WeipaiingState():
             else:
                 self.worksstate[int(i[1]) - 1][3] = input[1]
         if f == 0:
+            self.worksstate2 = worksstatetemp
             print('Null')
             return -1
-        return 1
+        return 0
 
     def BiddingMsgShow(self):
-        rstr = ''
-
+        rstr = '--本次拍品--\n'
         for i in self.worksstate:
-            stemp = str(i[0]) + ':' + i[1] + '。' + str(i[2]) + '元,' + i[3] + '\n'
+            stemp = str(i[0]) + ':' + i[1] + '。' + str(i[2]) + '元，' + i[3] + '\n'
             rstr += stemp
-
         print(rstr)
         return rstr
 
+
 class WeipaiedState():
     def __init__(self):
-        self.works=[]
-        self.worktemp=[]
-        self.menustate=0
-    def menu(self):
-        if self.menustate == 0:
-            rstr='--当前拍品--\n'
-            if self.works==[]:
-                rstr+='当前没有拍品\n'
+        self.wxid = ''
+        self.works = []
+        self.worktemp = []
+        self.EDmenustate = 0
+
+    def EDmenu(self):
+        if self.EDmenustate == 0:
+            rstr = '--当前拍品--\n'
+            if self.works == []:
+                rstr += '当前没有拍品\n'
             else:
                 for i in self.works:
-                    stemp = str(i[0]) + '、' + i[1] + ',' + str(i[2]) + '元' + '\n'
+                    stemp = str(i[0]) + '、' + i[1] + '，' + str(i[2]) + '元' + '\n'
                     rstr += stemp
             rstr += '[0]开始拍卖\n'
             rstr += '[1]添加拍品\n'
@@ -64,120 +68,222 @@ class WeipaiedState():
             rstr += '[3]删除拍品\n'
             rstr += '[4]退出\n'
             return rstr
-        elif self.menustate==11:
+        elif self.EDmenustate == 11:
             return '输入拍品名称'
-        elif self.menustate==12:
+        elif self.EDmenustate == 12:
             return '输入起拍价'
-        elif self.menustate==21:
+        elif self.EDmenustate == 21:
             return '输入拍品序号（输入#不修改）'
-        elif self.menustate==22:
+        elif self.EDmenustate == 22:
             return '输入拍品名称（输入#不修改）'
-        elif self.menustate==23:
+        elif self.EDmenustate == 23:
             return '输入起拍价（输入#不修改）'
-        elif self.menustate==31:
+        elif self.EDmenustate == 31:
             return '输入拍品序号(输入#退出)'
-    def ProcessingChangeMsg(self,input):
-        if self.menustate == 0:
+
+    def ChangeEDMsg(self, input):
+        if self.EDmenustate == 0:
             if input == '0':
-                pass
-                return 1
-            elif input == '1':
-                self.menustate=11
+                return 2
+            if input == '1':
+                self.EDmenustate = 11
                 return 0
             elif input == '2':
-                self.menustate=21
+                self.EDmenustate = 21
                 return 0
             elif input == '3':
-                self.menustate=31
+                self.EDmenustate = 31
                 return 0
+            elif input == '4':
+                return 1
             else:
                 return -1
 
-        elif self.menustate ==11:
-            self.worktemp.append(len(self.works)+1)
+        elif self.EDmenustate == 11:
+            self.worktemp.append(len(self.works) + 1)
             self.worktemp.append(input)
-            self.menustate=12
+            self.EDmenustate = 12
             return 0
-        elif self.menustate == 12:
+        elif self.EDmenustate == 12:
             self.worktemp.append(int(input))
             self.works.append(self.worktemp)
-            self.menustate = 0
-            self.worktemp=[]
+            self.EDmenustate = 0
+            self.worktemp = []
             return 0
 
-        elif self.menustate == 21:
-            if input=='#':
-                self.menustate=0
-                return 0
-            elif int(input)>0 and int(input)<=len(self.works):
-                self.worktemp.append(int(input))
-                self.menustate =22
-                return 0
-            else:
-                return -1
-        elif self.menustate == 22:
-            if input=='#':
-                self.worktemp.append(self.works[self.worktemp[0]-1][1])
-            else:
-                self.worktemp.append(input)
-            self.menustate =23
-            return 0
-        elif self.menustate == 23:
-            if input=='#':
-                self.worktemp.append(self.works[self.worktemp[0]-1][2])
-            else:
-                self.worktemp.append(int(input))
-            self.works[self.worktemp[0]-1]=self.worktemp
-            self.menustate =0
-            self.worktemp=[]
-            return 0
-
-        elif self.menustate==31:
-            if input=='#':
-                self.menustate=0
+        elif self.EDmenustate == 21:
+            if input == '#':
+                self.EDmenustate = 0
                 return 0
             elif int(input) > 0 and int(input) <= len(self.works):
-                del self.works[int(input)-1]
+                self.worktemp.append(int(input))
+                self.EDmenustate = 22
+                return 0
+            else:
+                return -1
+        elif self.EDmenustate == 22:
+            if input == '#':
+                self.worktemp.append(self.works[self.worktemp[0] - 1][1])
+            else:
+                self.worktemp.append(input)
+            self.EDmenustate = 23
+            return 0
+        elif self.EDmenustate == 23:
+            if input == '#':
+                self.worktemp.append(self.works[self.worktemp[0] - 1][2])
+            else:
+                self.worktemp.append(int(input))
+            self.works[self.worktemp[0] - 1] = self.worktemp
+            self.EDmenustate = 0
+            self.worktemp = []
+            return 0
+
+        elif self.EDmenustate == 31:
+            if input == '#':
+                self.EDmenustate = 0
+                return 0
+            elif int(input) > 0 and int(input) <= len(self.works):
+                del self.works[int(input) - 1]
                 for i in self.works:
-                    if i[0]>int(input):
-                        i[0]-=1
-                self.menustate =0
+                    if i[0] > int(input):
+                        i[0] -= 1
+                self.EDmenustate = 0
                 return 0
             else:
                 return -1
 
 
-
-
-
-
-class Weipai(WeipaiedState):
+class Weipai(WeipaiedState, WeipaiingState):
 
     def __init__(self):
 
+        self.state = 0  # 0 未拍卖  # 1 正在拍卖
+        self.TTmenustate = 0
+        self.rdata = [0, '', '']
+        self.admin = ['wxid_4v6rvyeygzfq22', 'wxid_z7m0kb9jpeeh22']
+        self.chatroom = '19753618745@chatroom'
 
-        self.state = [0]  # 0 未拍卖  # 1 正在拍卖
         WeipaiedState.__init__(self)
+        WeipaiingState.__init__(self)
+
+    def rpost(self):
+        if type(self.rdata[0])!=type([]):
+            if self.rdata[0] == 0:
+                data2 = {"type": 106, "user_id": self.rdata[1], "message": self.rdata[2], "msg_type": 1,
+                         "robot_id": "wxid_11pq0tkx9xk922",
+                         "token": "12skoko"}
+                rpoststr = json.dumps(data2, ensure_ascii=False).encode("utf-8")
+            else:
+                data3 = {"type": 101, "group_id": self.rdata[1], "message": self.rdata[2], "msg_type": 1,
+                         "robot_id": "wxid_11pq0tkx9xk922",
+                         "token": "12skoko"}
+                rpoststr = json.dumps(data3, ensure_ascii=False).encode("utf-8")
+            return rpoststr
+        else:
+            rpoststr=[]
+            for i in self.rdata:
+                if i[0] == 0:
+                    data2 = {"type": 106, "user_id": i[1], "message": i[2], "msg_type": 1,
+                             "robot_id": "wxid_11pq0tkx9xk922",
+                             "token": "12skoko"}
+                    rpoststr.append(json.dumps(data2, ensure_ascii=False).encode("utf-8"))
+                else:
+                    data3 = {"type": 101, "group_id": i[1], "message": i[2], "msg_type": 1,
+                             "robot_id": "wxid_11pq0tkx9xk922",
+                             "token": "12skoko"}
+                    rpoststr.append(json.dumps(data3, ensure_ascii=False).encode("utf-8"))
+            return rpoststr
+
+    def TTmenu(self):
+        if self.TTmenustate == 0:
+            return '--当前未在拍卖--\n[0]开始拍卖\n[1]查看拍品\n'
+        if self.TTmenustate == 10:
+            rstr = '本次拍品共有' + str(len(self.works)) + '件\n[0]开始拍卖\n[1]返回菜单\n'
+            return rstr
+
     def ProcessingMsg(self, cbdata):
-        if self.state == 0:
-            if cbdata['type'] == 100 and (cbdata['final_from_wxid'] == 'wxid_4v6rvyeygzfq22'):
-                pass
+        if self.state == 0 and cbdata['type'] == '100' and cbdata['final_from_wxid'] in self.admin:
+            if self.TTmenustate == 0:
+                if cbdata['msg'] == '菜单':
+                    self.rdata = [0, cbdata['final_from_wxid'], self.TTmenu()]
+                    return 1
+                elif cbdata['msg'] == '1':
+                    self.wxid = cbdata['final_from_wxid']
+                    self.TTmenustate = 11
+                    self.rdata = [0, cbdata['final_from_wxid'], self.EDmenu()]
+                    return 1
+                elif cbdata['msg'] == '0':
+                    if len(self.works) == 0:
+                        self.rdata = [0, cbdata['final_from_wxid'], '没有拍卖品']
+                        return 1
+                    else:
+                        self.TTmenustate = 10
+                        self.rdata = [0, cbdata['final_from_wxid'], self.TTmenu()]
+                        return 1
+                else:
+                    return 0
+            elif self.TTmenustate == 11:
+                if self.wxid == cbdata['final_from_wxid']:
+                    input = cbdata['msg']
+                    flag = self.ChangeEDMsg(input)
+                    if flag == -1:
+                        return 0
+                    elif flag == 0:
+                        self.rdata = [0, cbdata['final_from_wxid'], self.EDmenu()]
+                        print(self.EDmenu())
+                        return 1
+                    elif flag == 1:
+                        self.TTmenustate = 0
+                        self.rdata = [0, cbdata['final_from_wxid'], self.TTmenu()]
+                        return 1
+                    elif flag == 2:
+                        if len(self.works) == 0:
+                            self.rdata = [0, cbdata['final_from_wxid'], '没有拍卖品']
+                            return 1
+                        else:
+                            self.TTmenustate = 10
+                            self.rdata = [0, cbdata['final_from_wxid'], self.TTmenu()]
+                            return 1
+                else:
+                    self.rdata = [0, cbdata['final_from_wxid'], '当前菜单正在被使用']
+                    return 1
+            elif self.TTmenustate == 10:
+                if cbdata['msg'] == '0':
+                    self.state = 1
+                    self.TTmenustate = 0
+                    self.worksstate = self.works
+                    for i in self.worksstate:
+                        i.append('')
+                    self.rdata = [[0, self.chatroom, '拍卖开始！'],[0, cbdata['final_from_wxid'], '拍卖开始'],[0, self.chatroom, self.BiddingMsgShow()]]
+                    return 1
+                elif cbdata['msg'] == '1':
+                    self.TTmenustate = 0
+                    self.rdata = [0, cbdata['final_from_wxid'], self.TTmenu()]
+                    return 1
+                else:
+                    return 0
+            else:
+                return 0
+        elif self.state == 1 and cbdata['final_from_wxid'] in self.admin and cbdata['msg'] == '成交':
+            self.rdata = [[0, self.chatroom, self.BiddingMsgShow()],[0, self.chatroom, '本次拍卖结束，感谢大家参与'],[0, cbdata['final_from_wxid'], '拍卖结束'],[0, cbdata['final_from_wxid'], self.BiddingMsgShow()]]
+            self.worksstate = []
+            self.worksstate = []
+            self.works = []
+            self.state = 0
+            return 1
+
 
         elif self.state == 1:
-            pass
-
-
-
-
-
-
-
-
-
-
-
-
-
+            input = [cbdata['msg'], cbdata['final_from_name']]
+            flag = self.ProcessingBiddingMsg(input)
+            if flag == -1:
+                return 0
+            elif flag == 0:
+                self.rdata = [0, self.chatroom, self.BiddingMsgShow()]
+                return 1
+            return 0
+        else:
+            return 0
 
 
 app = Flask(__name__)
@@ -186,18 +292,21 @@ app = Flask(__name__)
 @app.route('/wxrobot', methods=['POST'])
 def wxrobot():
     cbdata = request.form.to_dict()
-
-    flag = wppp.ProcessingChangeMsg(cbdata['msg'])
+    print(cbdata)
+    flag = wppp.ProcessingMsg(cbdata)
 
     # data2 = {"type":106,"user_id":"wxid_4v6rvyeygzfq22","message":wppp.Weipaishow(),"msg_type":1,"robot_id":"wxid_11pq0tkx9xk922","token":"12skoko"}
     # data1 = {"type":106,"user_id":"wxid_4v6rvyeygzfq22","message":cbdata['message'],"msg_type":1,"robot_id":"wxid_11pq0tkx9xk922","token":"12skoko"}
 
-    if 1:
-        data3 = {"type": 101, "group_id": "19753618745@chatroom", "message": wppp.menu(), "msg_type": 1,
-                 "robot_id": "wxid_11pq0tkx9xk922", "token": "12skoko"}
-        rmsg = json.dumps(data3, ensure_ascii=False)
-        print(rmsg)
-        requests.post('http://127.0.0.1:3800', data=rmsg.encode("utf-8"))
+    if flag == 1:
+        rpoststr = wppp.rpost()
+        if type(rpoststr) != type([]):
+            print(rpoststr.decode())
+            requests.post('http://127.0.0.1:3800', data=rpoststr)
+        else:
+            for i in rpoststr:
+                print(i.decode())
+                requests.post('http://127.0.0.1:3800', data=i)
     return '200'
 
 
--- test4.py
+++ test4.py
@@ -1,45 +1,9 @@
 #!/usr/bin/python3
 #-*- coding:UTF-8 -*-
 
-class people:
-  name = ''
-  age = 0
-  __weight = 0
-  def __init__(self,n,a,w):
-    self.name = n
-    self.age = a
-    self.__weight = w
-
-  def speak(self):
-    print("%s 说：我%d岁。"%(self.name,self.age))
-
-#单继承实例
-class student(people):
-  grade = ''
-  def __init__(self,n,a,w,g):
-    people.__init__(self,n,a,w)
-    self.grade = g
-
-  def speak1(self):
-    print("%s说：我%d岁了，我在读%d年级。"%(self.name,self.age,self.grade))
-
-#另一个类
-class speaker():
-  topic = ''
-  name = ''
-  def __init__(self,n,t):
-    self.name = n
-    self.topic = t
-  def speak12(self):
-    print("我叫%s,我是一个演说家，我演讲的主题是%s"%(self.name,self.topic))
-
-
-#多重继承
-class sample(speaker,student):
-  a = ''
-  def __init__(self,n,a,w,g,t):
-    student.__init__(self,n,a,w,g)
-    speaker.__init__(self,n,t)
-
-test = sample("Tim",15,80,4,'python')
-test.speak()
+wppp=[0,'','']
+print(type(wppp[0]))
+wppp=[[0,'',''],[5]]
+print(type(wppp[0]))
+if type(wppp[0])==type([]):
+  print(1)
